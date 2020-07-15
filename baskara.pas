04b8b99885b2c11703037c80a29a3429f31ecf47
PedroTricossi <pedrodtricossi@gmail.com>
2020-07-15 13:17:33 -0300

    Exercicio de baskara

---
+++ baskara.pas
@@ -0,0 +1,12 @@
+program baskara(input, output);
+var b, c, raizdiscriminante: real;
+
+begin
+    read(b);
+    read(c);
+
+    raizdiscriminante := sqrt(b*b -4*c);
+
+    write((b - raizdiscriminante)/2);
+    write((b + raizdiscriminante)/2);
+end.
