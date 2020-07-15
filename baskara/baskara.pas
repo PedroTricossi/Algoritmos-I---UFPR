fdf7f29133556cc31aeecc43422353390c5e5ea5
PedroTricossi <pedrodtricossi@gmail.com>
2020-07-15 13:48:39 -0300

    Adicionada intevace para o usuario utilizar a solução de equações de segundo grau

--- baskara/baskara.pas
+++ baskara/baskara.pas
@@ -1,12 +1,19 @@
 program baskara(input, output);
-var b, c, raizdiscriminante: real;
+var a, b, c, raizdiscriminante: real;
 
 begin
-    read(b);
-    read(c);
+    write('Insira o valor de A:');
+    readln(a);
 
-    raizdiscriminante := sqrt(b*b -4*c);
+    write('Insira o valor de B:');
+    readln(b);
 
-    write((b - raizdiscriminante)/2);
-    write((b + raizdiscriminante)/2);
+    write('Insira o valor de C:');
+    readln(c);
+
+    raizdiscriminante := sqrt(b*b -4*a*c);
+
+    write('O valor da primeira raíz de X é:', (b - raizdiscriminante)/2*a);
+    write('O valor da segunda raíz de X é:', (b + raizdiscriminante)/2*a);
+
 end.
